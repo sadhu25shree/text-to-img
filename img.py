97bde483cb1c01adbf2c68816499d83a643a1bf9
sadhu25shree <159885096+sadhu25shree@users.noreply.github.com>
2024-08-22 22:11:40 +0530

    Update img.py

--- img.py
+++ img.py
@@ -1,28 +1,5 @@
 import requests
 import streamlit as st
-import base64
-
-
-def get_img_as_base64(file):
-    with open(file,"rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-img = get_img_as_base64("star.jpg")
-
-page_bg_img = f"""
-
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-background-image :url("data:image/png;base64,{img}");
-background-size : cover;
-}}
-[data-testid="stHeader"]{{
-background:rgba(0,0,0,0);
-}}
-</style>
-
-"""
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 
